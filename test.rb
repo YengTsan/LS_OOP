8368c165030098bad65f0bd91e4c29a08439fc94
YengTsan <Yengttt@gmail.com>
2016-04-20 13:33:44 +0800

    start to do exercise

--- test.rb
+++ test.rb
@@ -1,17 +1,62 @@
-class Animal
-  attr_accessor :name
+class Person
+  attr_accessor :name, :age
 
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def >(other_person)
+    age > other_person.age
+  end
+end
+
+bob = Person.new("Bob", 49)
+kim = Person.new("Kim", 33)
+
+class Team
+  attr_accessor :name, :members
   def initialize(name)
     @name = name
+    @members = []
+  end
+
+  def <<(person)
+    members.push person
+  end
+
+  def +(other_team)
+    temp_team = Team.new("Temporary Team")
+    temp_team.members = members + other_team.members
+    temp_team
+  end
+
+  def [](idx)
+    members[idx]
+  end
+
+  def []=(idx, obj)
+    members[idx] = obj
   end
 end
 
-class GoodDog < Animal
-  def initialize(color)
-    super
-    @color = color
-  end
-end
+# we'll use the same Person class from earlier
 
-p bruno = GoodDog.new("brown") 
-p Animal.ancestors
+cowboys = Team.new("Dallas Cowboys")
+cowboys.members << Person.new("Troy Aikman", 48)
+cowboys.members << Person.new("Emmitt Smith", 46)
+cowboys.members << Person.new("Michael Irvin", 49)
+
+
+niners = Team.new("San Francisco 49ers")
+niners.members << Person.new("Joe Montana", 59)
+niners.members << Person.new("Jerry Rice", 52)
+niners.members << Person.new("Deion Sanders", 47)
+
+p cowboys
+
+p niners
+
+dream_team = cowboys + niners  
+
+p dream_team[0]
